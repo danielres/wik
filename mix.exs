38fbb63023b3359793fc126df4e588a1150ae25f
Daniel Reszka <dannn.r@gmail.com>
2025-02-01 16:10:25 +0100

    deps: add earmark

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Wik.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:earmark, "~> 1.4"}
     ]
   end
 

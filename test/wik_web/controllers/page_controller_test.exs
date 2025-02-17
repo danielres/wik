63121b44d3c15fba9dcdbbc6652947dd8ae7bc32
Daniel Reszka <dannn.r@gmail.com>
2025-02-17 12:40:31 +0100

    PageControllerTest: fix

--- test/wik_web/controllers/page_controller_test.exs
+++ test/wik_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WikWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "data-telegram-login"
   end
 end

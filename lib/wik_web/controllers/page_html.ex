1f3fb32049f8fb0bbe836848c8f04b9d5a8cfe36
Daniel Reszka <dannn.r@gmail.com>
2025-02-22 12:33:41 +0100

    homepage: fix dev login crashing in prod
    * stores Mix.env() in a constant at compilation time
    
    * explanation: mix is not available at runtime in prod, so neither is Mix.env() , however it is available at compilation time.

--- lib/wik_web/controllers/page_html.ex
+++ lib/wik_web/controllers/page_html.ex
@@ -6,5 +6,12 @@ defmodule WikWeb.PageHTML do
   """
   use WikWeb, :html
 
+  @env Mix.env()
+
+  def show_dev_login? do
+    env = Atom.to_string(@env)
+    env in ["dev", "test"]
+  end
+
   embed_templates "page_html/*"
 end

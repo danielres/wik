950f560aea2b0aa5a0a4b606e4fb81cd73a598f8
Daniel Reszka <dannn.r@gmail.com>
2025-02-28 16:08:34 +0100

    dev login: fix non-serialized user.member_of exploding session cookie size
    * same as 8d848c1 but for local dev

--- lib/wik_web/controllers/session_controller.ex
+++ lib/wik_web/controllers/session_controller.ex
@@ -9,7 +9,18 @@ defmodule WikWeb.SessionController do
 
   def dev_login(conn, params) do
     username = params["user"]
-    groups = Wik.Groups.list_groups()
+
+    all_groups = Wik.Groups.list_groups()
+
+    serialized =
+      all_groups
+      |> Enum.map(fn group ->
+        %{
+          id: group.id,
+          name: group.name,
+          slug: group.slug
+        }
+      end)
 
     img =
       "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcBAMAAACAI8KnAAABg2lDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV9TpSKVDlYQcQhSneyioo61CkWoEGqFVh1Mrp/QpCFJcXEUXAsOfixWHVycdXVwFQTBDxB3wUnRRUr8X1JoEePBcT/e3XvcvQOERoWpZlcMUDXLSCXiYia7KgZeISCEAYxgRmamPidJSXiOr3v4+HoX5Vne5/4cfbm8yQCfSBxjumERbxBPb1o6533iMCvJOeJz4nGDLkj8yHXF5TfORYcFnhk20ql54jCxWOxgpYNZyVCJp4gjOVWjfCHjco7zFme1UmOte/IXBvPayjLXaQ4jgUUsQYIIBTWUUYGFKK0aKSZStB/38A85folcCrnKYORYQBUqZMcP/ge/uzULkxNuUjAOdL/Y9scoENgFmnXb/j627eYJ4H8GrrS2v9oAZj9Jr7e1yBEQ2gYurtuasgdc7gCDT7psyI7kpykUCsD7GX1TFui/BXrX3N5a+zh9ANLUVfIGODgExoqUve7x7p7O3v490+rvB903ctGnToVWAAAAIVBMVEVKHBxKHD5vKl68TqDKcrS8oE5OvLyFvE6eynK62Zv36fNZSEXTAAAAAWJLR0QAiAUdSAAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+kCDxUqHL67UiAAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAZklEQVQY02OYiQDT0mYy4OROSw1NQ+e6IIBbmgsBrrGxCULAGJ3LYGzitWoVkOm1aolLBzbuQkEg11GKMLcDL7cc2d5yIrjOxiBg4l6OyS0vBjKVjJWUjc2xcSECxiAeMVwEQOMCAM+GiLYSTcf8AAAAAElFTkSuQmCC"
@@ -21,7 +32,7 @@ defmodule WikWeb.SessionController do
       auth_date: "2023-01-01",
       username: username,
       photo_url: img,
-      member_of: groups
+      member_of: serialized
     }
 
     conn

b62dd1f3281b6e615ec0e63bf3c5ad070da22208
Daniel Reszka <dannn.r@gmail.com>
2025-11-16 16:23:50 +0100

    test_helper.exs: add (commented) sanity debug code

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,6 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Wik.Repo, :manual)
+
+# IO.puts("MIX_ENV=#{Mix.env()}")
+# IO.inspect(Application.get_env(:wik, Wik.Repo), label: "repo config")
+# IO.inspect(System.get_env("DATABASE_URL"), label: "DATABASE_URL")

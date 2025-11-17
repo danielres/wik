3c6629ef18adeae8de96bd752c5f508622db8cb1
Daniel Reszka <dannn.r@gmail.com>
2025-11-17 18:28:19 +0100

    OnlineUsersLive: simplify
    This might be a bit minimal and unoptimized, but should be fine
    for this app

--- lib/wik_web/components/online_users_live.ex
+++ lib/wik_web/components/online_users_live.ex
@@ -3,70 +3,37 @@ defmodule WikWeb.OnlineUsersLive do
 
   def mount(_params, _session, socket) do
     if connected?(socket), do: WikWeb.Presence.subscribe()
-    # Stream users into @streams.presences (does not track, only reads)
-    socket = stream(socket, :presences, WikWeb.Presence.list_online_users())
+    socket = socket |> assign(:presences, WikWeb.Presence.list_online_users())
     {:ok, socket}
   end
 
-  def handle_info({WikWeb.Presence, {:join, presence}}, socket) do
-    {:noreply, stream_insert(socket, :presences, presence)}
+  def handle_info({WikWeb.Presence, {:join, _presence}}, socket) do
+    socket = socket |> assign(:presences, WikWeb.Presence.list_online_users())
+    {:noreply, socket}
   end
 
-  def handle_info({WikWeb.Presence, {:leave, presence}}, socket) do
-    if presence.metas == [] do
-      {:noreply, stream_delete(socket, :presences, presence)}
-    else
-      {:noreply, stream_insert(socket, :presences, presence)}
-    end
+  def handle_info({WikWeb.Presence, {:leave, _presence}}, socket) do
+    socket = socket |> assign(:presences, WikWeb.Presence.list_online_users())
+    {:noreply, socket}
   end
 
-  def handle_info({WikWeb.Presence, {:update, %{id: id, meta: meta}}}, socket) do
-    presence = %{id: id, metas: [meta]}
-    {:noreply, stream_insert(socket, :presences, presence)}
+  def handle_info({WikWeb.Presence, {:update, %{id: _id, meta: _meta}}}, socket) do
+    socket = socket |> assign(:presences, WikWeb.Presence.list_online_users())
+    {:noreply, socket}
   end
-
-  def humanize_path(path) do
-    path
-    |> String.split("/")
-    |> Enum.map(&URI.decode(&1))
-    |> Enum.join("/")
-    |> remove_path_suffix()
-  end
-
-  def remove_path_suffix(path) do
-    path |> String.replace("::editing", "")
-  end
-
-  def deduplicate_metas(metas) do
-    metas
-    |> Enum.group_by(&base_path(&1.path))
-    |> Enum.flat_map(fn {_base, group} ->
-      {editing, non_editing} = Enum.split_with(group, &editing?/1)
-      n_replace = min(length(editing), length(non_editing))
-      keep_non_editing = Enum.drop(non_editing, n_replace)
-      keep_non_editing ++ editing
-    end)
-  end
-
-  defp base_path(path), do: String.replace_suffix(path, "::editing", "")
-
-  defp editing?(meta), do: String.ends_with?(meta.path, "::editing")
 
   def render(assigns) do
     ~H"""
-    <ul id="online_users" phx-update="stream" class="space-y-8">
-      <li :for={{dom_id, %{id: _id, metas: metas}} <- @streams.presences} id={dom_id}>
+    <ul id="online_users" class="space-y-8">
+      <li :for={%{id: id, metas: metas} <- @presences} id={id}>
         <div class="font-semibold">
-          {List.first(metas).username} <sup>{length(deduplicate_metas(metas))}</sup>
+          {List.first(metas).username} <sup>{length(metas)}</sup>
         </div>
         <ul class="pl-4">
-          <li :for={meta <- deduplicate_metas(metas)}>
-            <.link patch={meta.path |> remove_path_suffix()}>
-              {meta.path |> humanize_path()}
+          <li :for={meta <- metas}>
+            <.link navigate={meta.path}>
+              {meta.path}
             </.link>
-            <i :if={editing?(meta)} class="hero-pencil-solid  bg-slate-500 size-4 text-transparent">
-              (editing)
-            </i>
           </li>
         </ul>
       </li>

6406b9a1e8c618849b3094f2778dac45ba3655ff
Daniel Reszka <dannn.r@gmail.com>
2025-11-17 18:11:17 +0100

    core_components: header: adjust size and spacing

--- lib/wik_web/components/core_components.ex
+++ lib/wik_web/components/core_components.ex
@@ -288,9 +288,9 @@ defmodule WikWeb.CoreComponents do
 
   def header(assigns) do
     ~H"""
-    <header class={[@actions != [] && "flex items-center justify-between gap-6", "pb-4"]}>
+    <header class={["mb-8", @actions != [] && "flex items-center justify-between gap-6", "pb-4"]}>
       <div>
-        <h1 class="text-lg font-semibold leading-8">
+        <h1 class="text-xl font-semibold leading-8">
           {render_slot(@inner_block)}
         </h1>
         <p :if={@subtitle != []} class="text-sm text-base-content/70">

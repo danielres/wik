8fca12fca881e9a6b1e0f73aea13026a26ed7a3e
Daniel Reszka <dannn.r@gmail.com>
2025-11-17 18:12:19 +0100

    layout: remove application vsn

--- lib/wik_web/components/layouts.ex
+++ lib/wik_web/components/layouts.ex
@@ -50,7 +50,6 @@ defmodule WikWeb.Layouts do
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
         </a>
       </div>
       <div class="flex-none">

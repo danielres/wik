d38a3d92f8b5c61cb3a72d2baa7bf9fcdd622756
Daniel Reszka <dannn.r@gmail.com>
2025-11-20 20:29:21 +0100

    WikWeb.LiveUserAuth: add :group_membership_required

--- lib/wik_web/live_user_auth.ex
+++ lib/wik_web/live_user_auth.ex
@@ -5,6 +5,7 @@ defmodule WikWeb.LiveUserAuth do
   import Phoenix.Component
   import Phoenix.LiveView, only: [connected?: 1]
   use WikWeb, :verified_routes
+  use WikWeb, :live_view
 
   # This is used for nested liveviews to fetch the current user.
   # To use, place the following at the top of that liveview:
@@ -22,13 +23,27 @@ defmodule WikWeb.LiveUserAuth do
   end
 
   def on_mount(:live_user_required, _params, _session, socket) do
-    socket = socket |> assign_new(:ctx, fn -> %{} end)
-
     if socket.assigns[:current_user] do
-      ctx = Map.put(socket.assigns.ctx, :current_user, socket.assigns[:current_user])
-      {:cont, socket |> assign(:ctx, ctx)}
+      {:cont, socket |> ctx_add(:current_user, socket.assigns[:current_user])}
     else
       {:halt, Phoenix.LiveView.redirect(socket, to: ~p"/sign-in")}
+    end
+  end
+
+  def on_mount(:group_membership_required, %{"slug" => slug}, _session, socket) do
+    user = socket.assigns.current_user
+
+    # returns NotFoundError if policies don't allow access
+    case Wik.Accounts.Group
+         |> Ash.get(%{slug: slug}, actor: user) do
+      {:error, _} ->
+        {:halt,
+         socket
+         |> put_flash(:info, ~s( Group "#{slug}" not found ))
+         |> Phoenix.LiveView.redirect(to: ~p"/")}
+
+      {:ok, group} ->
+        {:cont, socket |> ctx_add(:current_group, group)}
     end
   end
 
@@ -41,14 +56,17 @@ defmodule WikWeb.LiveUserAuth do
   end
 
   def on_mount(:subscribe_presence, _params, _session, socket) do
-    socket = socket |> assign_new(:ctx, fn -> %{} end)
-
     if connected?(socket) do
       WikWeb.Presence.subscribe()
-      ctx = Map.put(socket.assigns.ctx, :presences, WikWeb.Presence.list_online_users())
-      {:cont, socket |> assign(:ctx, ctx)}
+      {:cont, socket |> ctx_add(:presences, WikWeb.Presence.list_online_users())}
     else
       {:cont, socket}
     end
+  end
+
+  defp ctx_add(socket, key, value) do
+    socket = socket |> assign_new(:ctx, fn -> %{} end)
+    ctx = Map.put(socket.assigns.ctx, key, value)
+    socket |> assign(ctx: ctx)
   end
 end

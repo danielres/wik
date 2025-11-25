4a18e777d91fe9b921772b14cd3bb5013030f44e
Daniel Reszka <dannn.r@gmail.com>
2025-11-25 23:45:16 +0100

    PageLive.Show: implement realtime updates

--- lib/wik_web/live/group_live/page_live/show.ex
+++ lib/wik_web/live/group_live/page_live/show.ex
@@ -8,13 +8,16 @@ defmodule WikWeb.GroupLive.PageLive.Show do
 
   use WikWeb, :live_view
   use WikWeb.Presence.Handlers
+  alias WikWeb.Toast
 
   @impl true
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} ctx={@ctx}>
       <.header>
-        {@page.title}
+        <div class={[:title in @updated_fields && "animate-reload"]}>
+          {@page.title}
+        </div>
 
         <:subtitle>
           <.link
@@ -52,7 +55,9 @@ defmodule WikWeb.GroupLive.PageLive.Show do
         >
         </.live_component>
       </.live_component>
-      <main>{@page.text}</main>
+      <main class={[:text in @updated_fields && "animate-reload"]}>
+        {@page.text}
+      </main>
     </Layouts.app>
     """
   end
@@ -66,10 +71,16 @@ defmodule WikWeb.GroupLive.PageLive.Show do
            load: [:versions_count]
          ) do
       {:ok, page} ->
+        if connected?(socket) do
+          Phoenix.PubSub.subscribe(Wik.PubSub, "page:updated:#{page.id}")
+          Phoenix.PubSub.subscribe(Wik.PubSub, "page:destroyed:#{page.id}")
+        end
+
         {:ok,
          socket
          |> assign(:page_title, page.title)
-         |> assign(:page, page)}
+         |> assign(:page, page)
+         |> assign(:updated_fields, [])}
 
       {:error, _error} ->
         page =
@@ -102,6 +113,52 @@ defmodule WikWeb.GroupLive.PageLive.Show do
       end
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "update", payload: payload}, socket) do
+    updated_page = reload_page!(payload.data.slug, socket)
+    updated_fields = Map.keys(payload.changeset.attributes)
+
+    if updated_fields == [] do
+      {:noreply, socket}
+    else
+      Process.send_after(self(), :clear_updated_fields, 2000)
+      msg = "Just updated by #{payload.actor}"
+      socket = socket |> put_flash(:info, msg)
+
+      socket =
+        socket
+        |> assign(
+          page: updated_page,
+          page_title: updated_page.title,
+          updated_fields: updated_fields
+        )
+
+      {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_info(:clear_updated_fields, socket) do
+    {:noreply, assign(socket, :updated_fields, [])}
+  end
+
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "destroy", payload: payload}, socket) do
+    msg = ~s(Page "#{payload.data.title}" was just deleted by #{payload.actor})
+    socket = socket |> Toast.put_toast(:info, msg)
+    socket = socket |> push_navigate(to: ~p"/#{socket.assigns.ctx.current_group.slug}/pages")
+    {:noreply, socket}
+  end
+
+  defp reload_page!(page_slug, socket) do
+    Wik.Wiki.Page
+    |> Ash.get!(
+      %{group_id: socket.assigns.ctx.current_group.id, slug: page_slug},
+      actor: socket.assigns.current_user,
+      load: [:versions_count]
+    )
   end
 
   defp has_editors?(socket, path) do

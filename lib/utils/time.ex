e1273c297b0f3f3178d741f39d0d17f89f08bd41
Daniel Reszka <dannn.r@gmail.com>
2025-11-25 20:07:08 +0100

    Utils.Time: add with relative() + absolute()

---
+++ lib/utils/time.ex
@@ -0,0 +1,34 @@
+defmodule Utils.Time do
+  def relative(date) when is_binary(date) do
+    {:ok, datetime} = Timex.parse(date, "{ISO:Extended}")
+    Timex.from_now(datetime)
+  end
+
+  def relative(%DateTime{} = date) do
+    Timex.from_now(date)
+  end
+
+  def absolute(date) when is_binary(date) do
+    {:ok, datetime} = Timex.parse(date, "{ISO:Extended}")
+    absolute(datetime)
+  end
+
+  def absolute(date) do
+    day = date.day
+    ordinal = ordinal_suffix(day)
+    month = Timex.format!(date, "{Mshort}")
+    year = date.year
+    time = Timex.format!(date, "{h24}:{0m}")
+
+    "#{month} #{day}#{ordinal}, #{year} - #{time}"
+  end
+
+  defp ordinal_suffix(day) do
+    case rem(day, 10) do
+      1 when day != 11 -> "st"
+      2 when day != 12 -> "nd"
+      3 when day != 13 -> "rd"
+      _ -> "th"
+    end
+  end
+end

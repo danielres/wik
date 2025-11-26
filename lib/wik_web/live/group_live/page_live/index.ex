16ec4348ad114977ae9159122806c11bcd8e43ca
Daniel Reszka <dannn.r@gmail.com>
2025-11-26 01:00:46 +0100

    PageLive.Index: implement realtime features

--- lib/wik_web/live/group_live/page_live/index.ex
+++ lib/wik_web/live/group_live/page_live/index.ex
@@ -1,5 +1,8 @@
 defmodule WikWeb.GroupLive.PageLive.Index do
   use WikWeb, :live_view
+  use WikWeb.Presence.Handlers
+  alias WikWeb.Components.RealtimeToast
+  require Ash.Query
 
   @impl true
   def render(assigns) do
@@ -15,6 +18,12 @@ defmodule WikWeb.GroupLive.PageLive.Index do
         rows={@streams.pages}
         row_click={
           fn {_id, page} -> JS.navigate(~p"/#{@ctx.current_group.slug}/pages/#{page.slug}") end
+        }
+        row_class={
+          fn {_id, page} ->
+            (page.id in @highlighted_page_ids && "animate-reload") ||
+              "hover:bg-base-200 transition"
+          end
         }
       >
         <:col :let={{_id, page}} label="Title">{page.title}</:col>
@@ -36,11 +45,38 @@ defmodule WikWeb.GroupLive.PageLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Wik.PubSub, "page:created")
+      Phoenix.PubSub.subscribe(Wik.PubSub, "page:updated")
+      Phoenix.PubSub.subscribe(Wik.PubSub, "page:destroyed")
+    end
+
+    pages = reload_pages!(socket)
+
     {:ok,
      socket
      |> assign(:page_title, "Listing Pages")
      |> assign_new(:current_user, fn -> nil end)
-     |> stream(:pages, Ash.read!(Wik.Wiki.Page, actor: socket.assigns[:current_user]))}
+     |> assign(:highlighted_page_ids, MapSet.new())
+     |> stream(:pages, pages)}
+  end
+
+  @impl true
+  def handle_params(_params, url, socket) do
+    WikWeb.Presence.track_in_liveview(socket, url)
+    {:noreply, socket}
+  end
+
+  defp reload_pages!(socket) do
+    current_group_id = socket.assigns.ctx.current_group.id
+
+    Wik.Wiki.Page
+    |> Ash.Query.filter(group_id == ^current_group_id)
+    |> Ash.read!(actor: socket.assigns[:current_user])
+  end
+
+  defp reload_page!(socket, id) do
+    Wik.Wiki.Page |> Ash.get!(id, actor: socket.assigns.current_user)
   end
 
   @impl true
@@ -48,6 +84,64 @@ defmodule WikWeb.GroupLive.PageLive.Index do
     page = Ash.get!(Wik.Wiki.Page, id, actor: socket.assigns.current_user)
     Ash.destroy!(page, actor: socket.assigns.current_user)
 
-    {:noreply, stream_delete(socket, :pages, page)}
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "create", payload: payload}, socket) do
+    # Only show pages for the current group
+    if payload.data.group_id == socket.assigns.ctx.current_group.id do
+      Process.send_after(self(), {:clear_highlight, payload.data.id}, 2000)
+
+      {:noreply,
+       socket
+       |> stream_insert(:pages, payload.data, at: 0)
+       |> update(:highlighted_page_ids, &MapSet.put(&1, payload.data.id))
+       |> RealtimeToast.put_create_toast(payload)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "destroy", payload: payload}, socket) do
+    if payload.data.group_id == socket.assigns.ctx.current_group.id do
+      socket =
+        socket
+        |> stream_delete(:pages, payload.data)
+        |> RealtimeToast.put_delete_toast(payload)
+
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_info(%Phoenix.Socket.Broadcast{event: "update", payload: payload}, socket) do
+    # Only show pages for the current group
+    if payload.data.group_id == socket.assigns.ctx.current_group.id do
+      Process.send_after(self(), {:clear_highlight, payload.data.id}, 2000)
+
+      socket =
+        socket
+        |> stream_insert(:pages, reload_page!(socket, payload.data.id))
+        |> update(:highlighted_page_ids, &MapSet.put(&1, payload.data.id))
+        |> RealtimeToast.put_update_toast(payload)
+
+      {:noreply, socket}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_info({:clear_highlight, page_id}, socket) do
+    page = reload_page!(socket, page_id)
+
+    {:noreply,
+     socket
+     |> update(:highlighted_page_ids, &MapSet.delete(&1, page_id))
+     |> stream_insert(:pages, page)}
   end
 end

42e3ffd497388953d7a7d7e30317014f63aa7e2b
Daniel Reszka <dannn.r@gmail.com>
2025-11-30 13:06:13 +0100

    CtxAdditions: add defensive programming + error logging, cleanup

--- lib/wik_web/ctx_additions.ex
+++ lib/wik_web/ctx_additions.ex
@@ -2,24 +2,39 @@ defmodule WikWeb.CtxAdditions do
   @moduledoc """
   Helpers for authenticating users in LiveViews.
   """
-  import Phoenix.Component
   use WikWeb, :verified_routes
   use WikWeb, :live_view
   require Ash.Query
+  require Logger
 
   # This is used for nested liveviews to fetch the current user.
   # To use, place the following at the top of that liveview:
   # on_mount {WikWeb.LiveUserAuth, :current_user}
 
-  def on_mount(:ctx_additions, params, _session, socket) do
-    current_group_id = socket.assigns.ctx.current_group.id
+  def on_mount(:ctx_additions, _params, _session, socket) do
+    current_group = socket.assigns.ctx.current_group
+    current_user = socket.assigns[:current_user]
 
     pages_map =
       Wik.Wiki.Page
-      |> Ash.Query.filter(group_id == ^current_group_id)
+      |> Ash.Query.filter(group_id == ^current_group.id)
       |> Ash.Query.sort(updated_at: :desc)
       |> Ash.Query.select([:id, :title, :slug, :updated_at])
-      |> Ash.read!(actor: socket.assigns[:current_user])
+      |> Ash.read(actor: current_user)
+      |> case do
+        {:ok, pages} ->
+          pages
+
+        {:error, error} ->
+          Logger.error("""
+          🔴 Failed to read pages for: 
+          Group: #{current_group} | id: #{current_group.id}
+          User: #{current_user} | id: #{current_user.id}
+          Error: #{Exception.format(:error, error)}
+          """)
+
+          []
+      end
 
     socket =
       socket

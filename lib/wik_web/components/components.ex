a9be45257289fe4af6508e20798face467ff18a9
Daniel Reszka <dannn.r@gmail.com>
2025-12-18 15:46:19 +0100

    WikWeb.Components.dropdown: add

--- lib/wik_web/components/components.ex
+++ lib/wik_web/components/components.ex
@@ -6,7 +6,34 @@ defmodule WikWeb.Components do
   - The `:content` slot renders tooltip content. 
   """
 
+  use WikWeb, :html
+
   use Phoenix.Component
+  attr :open?, :boolean, default: false
+  attr :class, :string, default: ""
+  attr :position, :string, default: "top"
+  attr :variant, :string, default: "neutral"
+  slot :inner_block, required: true
+  slot :content, required: true
+
+  def dropdown(assigns) do
+    ~H"""
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="cursor-pointer">{render_slot(@inner_block)}</div>
+
+      <div
+        tabindex="-1"
+        class="dropdown-content bg-base-300 backdrop-blur z-1 mt-2 rounded menu w-52 text-sm"
+      >
+        {render_slot(@content)}
+      </div>
+
+      {# <div tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"> }
+      {#   {render_slot(@content)} }
+      {# </div> }
+    </div>
+    """
+  end
 
   attr :open?, :boolean, default: false
   attr :class, :string, default: ""

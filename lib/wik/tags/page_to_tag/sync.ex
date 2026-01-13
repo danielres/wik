8567c0183ad48ab963744ac03ef64ca5ba2a37bf
Daniel Reszka <dannn.r@gmail.com>
2026-01-13 18:48:42 +0100

    fix "missed notifications" warnings

--- lib/wik/tags/page_to_tag/sync.ex
+++ lib/wik/tags/page_to_tag/sync.ex
@@ -23,14 +23,15 @@ defmodule Wik.Tags.PageToTag.Sync do
       |> Enum.uniq()
 
     Wik.Repo.transaction(fn ->
-      tag_ids =
-        tags
-        |> Enum.map(&upsert_tag!(group_id, &1))
-
-      replace_page_tags!(group_id, page_id, tag_ids)
+      {tag_ids, notifications} = upsert_tags(group_id, tags)
+      replace_page_tags!(group_id, page_id, tag_ids, notifications)
     end)
     |> case do
-      {:ok, _} ->
+      {:ok, notifications} ->
+        if notifications != [] do
+          Ash.Notifier.notify(notifications)
+        end
+
         :ok
 
       {:error, reason} ->
@@ -44,39 +45,48 @@ defmodule Wik.Tags.PageToTag.Sync do
     end
   end
 
-  defp upsert_tag!(group_id, name) do
-    {:ok, tag} =
-      Tag
-      |> Ash.Changeset.for_create(:create, %{group_id: group_id, name: name})
-      |> Ash.create(
-        upsert?: true,
-        upsert_identity: :unique_group_name,
-        upsert_fields: [:name],
-        authorize?: false,
-        return_notifications?: false
-      )
+  defp upsert_tags(group_id, tags) do
+    {tag_ids, notifications} =
+      Enum.reduce(tags, {[], []}, fn name, {ids, notifications} ->
+        {:ok, tag, new_notifications} =
+          Tag
+          |> Ash.Changeset.for_create(:create, %{group_id: group_id, name: name})
+          |> Ash.create(
+            upsert?: true,
+            upsert_identity: :unique_group_name,
+            upsert_fields: [:name],
+            authorize?: false,
+            return_notifications?: true
+          )
 
-    tag.id
+        {[tag.id | ids], notifications ++ new_notifications}
+      end)
+
+    {Enum.reverse(tag_ids), notifications}
   end
 
-  defp replace_page_tags!(group_id, page_id, tag_ids) do
-    PageToTag
-    |> filter(group_id == ^group_id and page_id == ^page_id)
-    |> Ash.bulk_destroy!(:destroy, %{}, authorize?: false, return_notifications?: false)
+  defp replace_page_tags!(group_id, page_id, tag_ids, notifications) do
+    bulk_result =
+      PageToTag
+      |> filter(group_id == ^group_id and page_id == ^page_id)
+      |> Ash.bulk_destroy!(:destroy, %{}, authorize?: false, return_notifications?: true)
 
-    Enum.each(tag_ids, fn tag_id ->
+    notifications = notifications ++ bulk_result.notifications
+
+    Enum.reduce(tag_ids, notifications, fn tag_id, notifications ->
       case PageToTag
            |> Ash.Changeset.for_create(:create, %{
              group_id: group_id,
              page_id: page_id,
              tag_id: tag_id
            })
-           |> Ash.create(authorize?: false, return_notifications?: false) do
-        {:ok, _} -> :ok
-        {:error, reason} -> raise "Failed to create PageToTag: #{inspect(reason)}"
+           |> Ash.create(authorize?: false, return_notifications?: true) do
+        {:ok, _page_to_tag, new_notifications} ->
+          notifications ++ new_notifications
+
+        {:error, reason} ->
+          raise "Failed to create PageToTag: #{inspect(reason)}"
       end
     end)
-
-    :ok
   end
 end
